a2a3ce1b2f84caf5c4db1cd73a360a2b9740ea83
Nurzhan <103424576+NikonFalse@users.noreply.github.com>
2023-11-07 18:25:22 +0500

    Test4.java

--- mod1/src/test/java/spring_introduction/Test4.java
+++ mod1/src/test/java/spring_introduction/Test4.java
@@ -6,7 +6,7 @@ public class Test4 {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context =
                 new ClassPathXmlApplicationContext("applicationContext2");
-
+        
         context.close();
 
     }
